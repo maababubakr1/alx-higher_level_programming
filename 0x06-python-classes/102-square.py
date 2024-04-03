0f2c82ab57638530c1eded18fc68cc38e917712f
maababubakr1 <maababubakr1@gmail.com>
2024-04-04 00:54:33 +0200

    nnn

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """initialize a new square
+
+        Args:
+        size (int): The size of new Square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get/set the current size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int, float):
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return the current area of the square"""
+        return (self.__size * self.__size)
+
+    def __eq__(self, other):
+        """ define == comparaison"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """ define 1= comparaison"""
+        return self.area() != other.area()
+
+    def __It__(self, other):
+        """ define < comparaison"""
+        return self.area() < other.area()
+
+    def __gt__(self, other):
+        """ define > comparaison"""
+        return self.area() > other.area()
+
+    def __le__(self, other):
+        """ define <= comparaison"""
+        return self.area() <= other.area()
+
+    def __ge__(self, other):
+        """ define >= comparaison"""
+        return self.area() >= other.area()
