ddf32d75fce8d20605b83d44815c8d615376482a
maababubakr1 <maababubakr1@gmail.com>
2024-05-23 01:37:07 +0200

    fixation1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- Creates the user user_0d_1 with all privileges
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
